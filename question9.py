04843439a53a0c180a32f29aa016225d23af72dc
kareena shaik <shaikkareena44@gmail.com>
2026-08-11 11:51:20 +0530

    Completed question 9

---
+++ question9.py
@@ -0,0 +1,7 @@
+text="PythonProgramming"
+print("First 5 Characters:",text[0:5])
+print("ast 5 Characters:",text[-5:])
+print("Characters from Index 2 to 7:",text[2:8])
+print("Every Second Character:",text[0::2])
+print("Message in Reverse:",text[::-1])
+print("Message Without First and Last Character:",text[1:-1])
